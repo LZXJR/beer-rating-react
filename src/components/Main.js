79162f8ea584668ad9890d1ecc3d77d8409101b3
LZXJR <ostapdiachok@gmail.com>
2021-05-04 19:13:03 +0300

    Added block main static

---
+++ src/components/Main.js
@@ -0,0 +1,16 @@
+const Main = () => {
+  return (
+    <div className="container">
+      <main>
+          <div className="rating_title">
+              <div className="rating_title_item">№</div>
+              <div className="rating_title_item">Name</div>
+              <div className="rating_title_item">Producer</div>
+              <div className="rating_title_item">Grade</div>
+          </div>
+      </main>
+    </div>
+  );
+};
+
+export default Main
