2fa7bdd06ba251b253a513318af03dd2dc679c02
LZXJR <ostapdiachok@gmail.com>
2021-05-04 17:28:23 +0300

    Added header static

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,28 @@
+import beersIcon from "../img/header/beersIcon.svg";
+import searchIcon from "../img/header/searchIcon.svg";
 
+const Header = () => {
+  return (
+    <header>
+      <div className="container">
+        <div className="header">
+          <div className="header_title">
+            <h1>BeerRating</h1>
+            <img src={beersIcon} alt="beers"></img>
+          </div>
+          <div className="header_searchbar">
+            <input
+              type="search"
+              className="header_searchbar_item"
+              placeholder="Search..."
+            ></input>
+            <img src={searchIcon} alt="search" className="search_icon"></img>
+            
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
