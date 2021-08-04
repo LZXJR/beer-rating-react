f6330cb6047de6842a9eaa10c04103af6a0b83c9
LZXJR <ostapdiachok@gmail.com>
2021-08-04 20:12:26 +0300

    Composition of main page changed

--- src/features/rating/ui/Rating.jsx
+++ src/features/rating/ui/Rating.jsx
@@ -1,8 +1,10 @@
 import { useTranslation } from "react-i18next";
-import "./style.css"
+import "./style.css";
 
-export const Rating = ({children}) => {
-  const {t}=useTranslation()
+import {RatingItem} from "../../../shared/ratingItem/ui/RatingItem"
+
+export const Rating = ({ children }) => {
+  const { t } = useTranslation();
   return (
     <div className="rating">
       <div className="rating_title">
@@ -12,9 +14,73 @@ export const Rating = ({children}) => {
         <div className="rating_title_item">{t("ratingTitle.country")}</div>
         <div className="rating_title_item">{t("ratingTitle.grade")}</div>
       </div>
-
+      <RatingItem
+        place="1"
+        name="Stella Artois"
+        producer="Anheuser-Busch InBev"
+        country="Belgium"
+      />
+      <RatingItem
+        place="1"
+        name="Stella Artois"
+        producer="Anheuser-Busch InBev"
+        country="Бельгія"
+      />
+      <RatingItem
+        place="1"
+        name="Stella Artois"
+        producer="Anheuser-Busch InBev"
+        country="Бельгія"
+      />
+      <RatingItem
+        place="1"
+        name="Stella Artois"
+        producer="Anheuser-Busch InBev"
+        country="Бельгія"
+      />
+      <RatingItem
+        place="1"
+        name="Stella Artois"
+        producer="Anheuser-Busch InBev"
+        country="Бельгія"
+      />
+      <RatingItem
+        place="1"
+        name="Stella Artois"
+        producer="Anheuser-Busch InBev"
+        country="Бельгія"
+      />
+      <RatingItem
+        place="1"
+        name="Stella Artois"
+        producer="Anheuser-Busch InBev"
+        country="Бельгія"
+      />
+      <RatingItem
+        place="1"
+        name="Stella Artois"
+        producer="Anheuser-Busch InBev"
+        country="Бельгія"
+      />
+      <RatingItem
+        place="1"
+        name="Stella Artois"
+        producer="Anheuser-Busch InBev"
+        country="Бельгія"
+      />
+      <RatingItem
+        place="1"
+        name="Stella Artois"
+        producer="Anheuser-Busch InBev"
+        country="Бельгія"
+      />
+      <RatingItem
+        place="1"
+        name="Stella Artois"
+        producer="Anheuser-Busch InBev"
+        country="Бельгія"
+      />
       {children}
     </div>
   );
 };
-
--- src/pages/main/ui/MainPage.jsx
+++ src/pages/main/ui/MainPage.jsx
@@ -1,37 +1,25 @@
 import { Suspense } from "react";
-import "./style.css"
+import "./style.css";
 
+import { Layout } from "../../../shared/layout/ui/Layout";
 import { Header } from "../../../shared/header/Header.jsx";
-import { Main } from "../../../shared/main/index";
-import {Rating} from "../../../features/rating/ui/Rating.jsx";
-import {RatingItem} from "../../../shared/ratingItem/index"
-import {NavBar} from "../../../shared/navBar/NavBar.jsx";
+import { Rating } from "../../../features/rating/ui/Rating.jsx";
+import { NavBar } from "../../../shared/navBar/NavBar.jsx";
 
-export const MainPage = () =>{
-    return(
-        <>
-        <Suspense fallback="Loading...">
+export const MainPage = () => {
+  return (
+    <>
+      <Suspense fallback="Loading...">
         <NavBar />
         <div className="header_and_main">
           <Header />
-          <Main>
-            <Rating>
-            <RatingItem place="1" name="Stella Artois" producer="Anheuser-Busch InBev" country="Belgium"/>
-            <RatingItem place="1" name="Stella Artois" producer="Anheuser-Busch InBev" country="Бельгія"/>
-            <RatingItem place="1" name="Stella Artois" producer="Anheuser-Busch InBev" country="Бельгія"/>
-            <RatingItem place="1" name="Stella Artois" producer="Anheuser-Busch InBev" country="Бельгія"/>
-            <RatingItem place="1" name="Stella Artois" producer="Anheuser-Busch InBev" country="Бельгія"/>
-            <RatingItem place="1" name="Stella Artois" producer="Anheuser-Busch InBev" country="Бельгія"/>
-            <RatingItem place="1" name="Stella Artois" producer="Anheuser-Busch InBev" country="Бельгія"/>
-            <RatingItem place="1" name="Stella Artois" producer="Anheuser-Busch InBev" country="Бельгія"/>
-            <RatingItem place="1" name="Stella Artois" producer="Anheuser-Busch InBev" country="Бельгія"/>
-            <RatingItem place="1" name="Stella Artois" producer="Anheuser-Busch InBev" country="Бельгія"/>
-            <RatingItem place="1" name="Stella Artois" producer="Anheuser-Busch InBev" country="Бельгія"/>
-
-            </Rating>
-          </Main>
-          </div>
+          <main>
+            <Layout>
+              <Rating />
+            </Layout>
+          </main>
+        </div>
       </Suspense>
-        </>
-    )
-} 
+    </>
+  );
+};
