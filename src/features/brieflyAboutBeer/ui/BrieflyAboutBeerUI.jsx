32f1330ec12300f3e8f7138b72d140d194334fc3
LZXJR <ostapdiachok@gmail.com>
2021-08-18 21:25:56 +0300

    BeerCharacteristicsItem evaluation logic added

--- src/features/brieflyAboutBeer/ui/BrieflyAboutBeerUI.jsx
+++ src/features/brieflyAboutBeer/ui/BrieflyAboutBeerUI.jsx
@@ -5,6 +5,13 @@ import "./style.css";
 
 import beerPhoto from "./img/stella-beer.png";
 
+import costIcon from "./img/money-bag.svg"
+import tasteIcon from "./img/wine-tasting.svg"
+import lookIcon from "./img/look.svg"
+import SmellIcon from "./img/smell.svg"
+import alcoholIcon from "./img/alcohol-content.svg"
+
+
 export const BrieflyAboutBeerUI = () => {
   const { t } = useTranslation();
   return (
@@ -34,24 +41,33 @@ export const BrieflyAboutBeerUI = () => {
           <BeerCharacteristicsItem
             characteristicTitle={t("BeerCharacteristicsItem.titleCost")}
             characteristicTag="cost"
+            characteristicIcon={costIcon}
           />
           <BeerCharacteristicsItem
             characteristicTitle={t("BeerCharacteristicsItem.titleTaste")}
             characteristicTag="taste"
+            characteristicIcon={tasteIcon}
+
           />
           <BeerCharacteristicsItem
             characteristicTitle={t("BeerCharacteristicsItem.titleLook")}
             characteristicTag="look"
+            characteristicIcon={lookIcon}
+
           />
           <BeerCharacteristicsItem
             characteristicTitle={t("BeerCharacteristicsItem.titleSmell")}
             characteristicTag="smell"
+            characteristicIcon={SmellIcon}
+
           />
           <BeerCharacteristicsItem
             characteristicTitle={t(
               "BeerCharacteristicsItem.titleAlcoholContent"
             )}
             characteristicTag="alcohol content"
+            characteristicIcon={alcoholIcon}
+
           />
         </div>
       </div>
--- src/shared/BeerCharacteristicsItem/ui/BeerCharacteristicsItemUI.jsx
+++ src/shared/BeerCharacteristicsItem/ui/BeerCharacteristicsItemUI.jsx
@@ -1,7 +1,11 @@
 import "./style.css";
-import characteristicIcon from "./img/look.svg" 
 
-export const BeerCharacteristicsItem = ({characteristicTitle,characteristicTag,characteristicGrade}) => {
+export const BeerCharacteristicsItem = ({
+  characteristicTitle,
+  characteristicTag,
+  characteristicGrade,
+  characteristicIcon,
+}) => {
   return (
     <div className="beer_characteristics_item">
       <img
@@ -10,14 +14,44 @@ export const BeerCharacteristicsItem = ({characteristicTitle,characteristicTag,c
         alt="beer_characteristic"
       />
       <div className="beer_characteristics_item_scale_and_title">
-      <p>{characteristicTitle}</p>
-      <div className="beer_characteristics_item_scale">
-        <div className={"beer_characteristics_item_scale_item " + characteristicTag}  />
-        <div className={"beer_characteristics_item_scale_item " + characteristicTag}  />
-        <div className={"beer_characteristics_item_scale_item " + characteristicTag}  />
-        <div className={"beer_characteristics_item_scale_item " + characteristicTag}  />
-        <div className={"beer_characteristics_item_scale_item " + characteristicTag}  />
-      </div>
+        <p>{characteristicTitle}</p>
+        <div className="beer_characteristics_item_scale">
+          <div
+            className={
+              characteristicGrade >= 1
+                ? "beer_characteristics_item_scale_item " + characteristicTag
+                : "beer_characteristics_item_scale_item "
+            }
+          />
+          <div
+            className={
+              characteristicGrade >= 2
+                ? "beer_characteristics_item_scale_item " + characteristicTag
+                : "beer_characteristics_item_scale_item "
+            }
+          />
+          <div
+            className={
+              characteristicGrade >= 3
+                ? "beer_characteristics_item_scale_item " + characteristicTag
+                : "beer_characteristics_item_scale_item "
+            }
+          />
+          <div
+            className={
+              characteristicGrade >= 4
+                ? "beer_characteristics_item_scale_item " + characteristicTag
+                : "beer_characteristics_item_scale_item "
+            }
+          />
+          <div
+            className={
+              characteristicGrade >= 5
+                ? "beer_characteristics_item_scale_item " + characteristicTag
+                : "beer_characteristics_item_scale_item "
+            }
+          />
+        </div>
       </div>
     </div>
   );
