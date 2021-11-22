b3641cb9665e59a2e5782ed4fff818b069961b1f
LZXJR <ostapdiachok@gmail.com>
2021-11-22 15:19:28 +0200

    Ready to deploy

--- src/shared/beerRatingApi.js
+++ src/shared/beerRatingApi.js
@@ -1,9 +1,12 @@
 import axios from "axios";
 
-const IP = "localhost";
-const PORT = 5000;
+// const IP = "localhost";
+// const PORT = 5000;
 
-const homePageUrl = `http://${IP}:${PORT}`;
+// const homePageUrl = `http://${IP}:${PORT}`;
+
+const homePageUrl = `https://beer-rating-api.herokuapp.com`;
+
 
 export const beerRatingApi = (path, id, method = "GET", data) => {
 
